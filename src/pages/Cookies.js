74a6be9694e8988f72e5d4b581b34e3183fb3449
Sean Nugent <ikagara@live.com>
2025-02-07 10:10:42 +0000

    Add cookies page

---
+++ src/pages/Cookies.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '@scottish-government/design-system/dist/css/design-system.min.css';
+
+const About = () => {
+  return (
+    <div className="ds_page__middle">
+      <div className="ds_wrapper">
+        <main id="main-content">
+        <img
+                width="300"
+                height="300"
+                src="https://upload.wikimedia.org/wikipedia/commons/f/f1/2ChocolateChipCookies.jpg"
+                alt="Cookies"
+              />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default About;
