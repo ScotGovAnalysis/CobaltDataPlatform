0fdc546e426c77337bd0d2386215278bae46e951
seanjnugent <87038678+seanjnugent@users.noreply.github.com>
2025-02-21 09:21:56 +0000

    Update Privacy.js

--- src/pages/Privacy.js
+++ src/pages/Privacy.js
@@ -11,52 +11,30 @@ const Privacy = () => {
               <div className="ds_cb__text ds_cb__content ds_cb__text--center">
                 <h1 className="ds_page-header__title">Search Results</h1>
                 <p className="ds_lead ds_lead--spacious">
-                  Explore the datasets matching your search criteria.
-                </p>
-                <div className="ds_site-search ds_site-search--large ds_site-search--spacious ds_site-search--with-border">
-                  <form action="/search" role="search" className="ds_site-search__form" method="GET">
-                    <label className="ds_label visually-hidden" htmlFor="site-search">Search</label>
-                    <div className="ds_input__wrapper ds_input__wrapper--has-icon ds_input__wrapper--bordered">
-                      <input
-                        className="ds_input ds_site-search__input"
-                        id="site-search"
-                        name="q"
-                        placeholder="Search here"
-                        type="search"
-                      />
-                      <button type="submit" className="ds_button ds_button--icon-only ds_button--search ds_button--square">
-                        <span className="visually-hidden">Search</span>
-                        <svg className="ds_icon ds_icon--magnifying-glass" aria-hidden="true" role="img">
-                          <use href="/webfiles/1737461086955/assets/images/icons/icons.stack.svg#magnifying-glass"></use>
-                        </svg>
-                      </button>
-                    </div>
-                  </form>
-                </div>
-                <p className="ds_hint-text ds_hint-text--spacious">For example, "population estimates" or "economy".</p>
-              </div>
-              <div className="ds_results">
-                <h2>Results</h2>
-                <ul className="ds_results__list">
-                  <li className="ds_results__item">
-                    <h3 className="ds_results__item-title">Dataset 1</h3>
-                    <p className="ds_results__item-description">Description of dataset 1.</p>
-                    <a href="#" className="ds_results__item-link">View Dataset</a>
-                  </li>
-                  <li className="ds_results__item">
-                    <h3 className="ds_results__item-title">Dataset 2</h3>
-                    <p className="ds_results__item-description">Description of dataset 2.</p>
-                    <a href="#" className="ds_results__item-link">View Dataset</a>
-                  </li>
-                  {/* Add more dataset items as needed */}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </main>
-      </div>
-    </div>
-  );
-};
+Privacy
+
+We collect and process the following information when you visit statistics.gov.scot:
+
+any feedback you share with us
+
+your cookies using Google Analytics
+
+your IP address
+
+We also use Google Analytics to collect information about:
+
+which type of browser and device you used
+
+the pages you visit on statistics.gov.scot
+
+how long you spend on each statistics.gov.scot page
+
+how you got to the site
+
+what you click on while you're visiting the site
+
+We use the information to improve our service. Google Analytics data is only collected from users who have consented to share this data. Any information we get about you from Google Analytics will be anonymised before we see it. This means we will not hold any personally identifiable information about you, such as your name or address.        
+  </p>
+             
 
 export default Privacy;
