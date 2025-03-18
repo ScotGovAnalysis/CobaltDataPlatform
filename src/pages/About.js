fefb5b4952b33ebf33b1f33cd5a09ce138dc6103
Sean Nugent <ikagara@live.com>
2025-03-18 13:53:22 +0000

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -25,8 +25,7 @@ const About = () => {
           <p className="ds_h3">
               Cobalt Open Data Portal
             </p>
-            <p>
- ABNOUT TEXT
+            <p>The Scottish Government’s Open Data team are running an Agile alpha to build prototypes to test different ideas and explore new approaches for statistics.gov.scot. Prototypes will be focused on addressing the high priority user needs that were identified during discovery, as well as addressing the key problems and challenges our users face. This Cobalt Open Data Portal is one of those prototypes. 
               </p>
           {/* Introduction Section */}
           <section className="ds_layout ds_layout--article">
@@ -60,7 +59,11 @@ const About = () => {
             <div className="ds_layout__content">
               <h2 className="ds_h3">History</h2>
               <p>
-sdgfd
+In 2015, the Scottish Government published an Open Data Strategy to help achieve its data vision – a Scotland which recognises the value of data and responsibly make use of data to improve public services and deliver wider societal and economic benefits for all. A key National Action to emerge from the strategy was the scoping and establishment of a Scottish Data Discovery Site to be in place for early 2016. 
+
+In February 2016, the Scottish Government launched Statistics.gov.scot, a new site for publishing the Official Statistics of Scotland and the data behind them. The site is actively managed by the Scottish Government’s Open Data Team that sits within the Digital Directorate’s Data Division. 
+
+The Scottish Government made a commitment to review the site as part of Scotland’s Open Government Action Plan 2021-25 to ensure the site is meeting user needs. In 2024, the Open Data team completed a discovery aimed better understanding statistics.gov.scot. In 2025, we published a blog updating you on our progress as well as the full report on the programme of user research completed as part of the 2024 discovery.
               </p>
             </div>
           </section>
